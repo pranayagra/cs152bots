87d95fe12702a773d45fe60560082216255d4037
Tathagat Verma <tathagatswagverma@gmail.com>
2023-05-31 23:48:56 -0700

    added requirements

--- DiscordBot/utils.py
+++ DiscordBot/utils.py
@@ -2,6 +2,19 @@ import discord
 from discord.utils import get
 from discord.ext import commands
 from enum import Enum, auto
+import openai
+import os
+import json
+
+token_path = 'tokens.json'
+if not os.path.isfile(token_path):
+    raise Exception(f"{token_path} not found!")
+with open(token_path) as f:
+    # If you get an error here, it means your token is formatted incorrectly. Did you put it in quotes?
+    tokens = json.load(f)
+    discord_token = tokens['discord']
+    openai.organization = tokens['openai.organization']
+    openai.api_key = tokens['openai.api_key']
 
 DEBUG = True
 
@@ -20,6 +33,9 @@ def get_category_by_name(guild, category_name):
             return category
     return None
 
+def message_autoflag(message):
+    print(openai.Model.list())
+
 class BadUserState(Enum):
     SUSPEND = auto()
     WARN = auto()
@@ -27,3 +43,6 @@ class BadUserState(Enum):
 
 def url_to_text(url):
     print(requests.get("http://stackoverflow.com").text)
+
+if __name__ == '__main__':
+    message_autoflag('aaaaaa')
